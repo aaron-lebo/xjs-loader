78f9befde0e9ae09c860283dde25f122467013d8
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2016-04-03 02:50:08 +0000

    simplified

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-var escodegen = require('escodegen');
 var execSync = require('child_process').execSync;
 
 module.exports = function(source) {
-    var res = execSync('mix xjs ' + this.resourcePath);
-    var ast = JSON.parse(res);
-    return escodegen.generate(ast);
+    return execSync('mix xjs ' + this.resourcePath);
 };
